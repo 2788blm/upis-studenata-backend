6b69da617a09af1d43190a0c19e530f810a7ac3c
2788blm <2711mm@gmail.com>
2022-05-13 14:46:47 +0200

    Project created

---
+++ src/main/java/com/dodeka/upisstudenatabackend/UpisStudenataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dodeka.upisstudenatabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpisStudenataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpisStudenataBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dodeka/upisstudenatabackend/UpisStudenataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodeka.upisstudenatabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpisStudenataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
