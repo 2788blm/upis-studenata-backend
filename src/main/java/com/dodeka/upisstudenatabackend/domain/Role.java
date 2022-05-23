8c567b55a11a99494f430c45b2214b75f5d79dd6
2788blm <2711mm@gmail.com>
2022-05-23 08:26:22 +0200

    User entity added

---
+++ src/main/java/com/dodeka/upisstudenatabackend/domain/Role.java
@@ -0,0 +1,5 @@
+package com.dodeka.upisstudenatabackend.domain;
+
+public enum Role {
+    STUDENT, ADMINISTRATOR, SEKRETAR
+}
--- src/main/java/com/dodeka/upisstudenatabackend/domain/User.java
+++ src/main/java/com/dodeka/upisstudenatabackend/domain/User.java
@@ -5,22 +5,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class SkolskaGodina {
-    
-    @Id
-    private String godina;  // 2021/2022
+public class User {
 
-    @OneToMany(mappedBy = "predmet", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Predmet> predmeti;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+
+    private String password;
+
+    private Role role;
+
 }
